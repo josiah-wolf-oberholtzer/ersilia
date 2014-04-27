9290ec0850fe1a9c5864852fbbcfd72b57b293a2
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-04-27 18:35:13 -0400

    Updated root metadata module.

--- __metadata__.py
+++ __metadata__.py
@@ -3,5 +3,7 @@ import collections
 
 
 metadata = collections.OrderedDict([
+    ('forces_tagline', 'for Dal Niente'),
     ('title', 'Invisible Cities: Ersilia'),
+    ('year_of_completion', 2014),
     ])
