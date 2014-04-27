f8586cee689f7376719950caf1808f62a6282b0e
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-04-27 18:35:26 -0400

    Added bare instrumentation material package.

---
+++ materials/instrumentation/definition.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+output_module_import_statements = [
+    'from abjad import *',
+    ]
+
+
+instrumentation = instrumenttools.InstrumentationSpecifier()
---
+++ materials/instrumentation/output.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+instrumentation = instrumenttools.InstrumentationSpecifier(
+    performers=instrumenttools.PerformerInventory(
+        []
+        ),
+    )
