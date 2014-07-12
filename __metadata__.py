629309d066aee14dd3ee284f91fd68ea35a8a4ab
josiah-wolf-oberholtzer <josiah.oberholtzer@gmail.com>
2014-07-12 17:02:51 -0400

    Added missing files.

--- __metadata__.py
+++ __metadata__.py
@@ -5,6 +5,6 @@ import collections
 metadata = collections.OrderedDict([
     ('composer', 'Oberholtzer'),
     ('forces_tagline', 'for Dal Niente'),
-    ('title', 'Invisible Cities: Ersilia'),
+    ('title', 'Invisible Cities 3: Ersilia'),
     ('year', 2014),
     ])
