555cc1a0074234d8204ba022753e8a21a00fc981
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-11 13:36:37 -0800

    Removed unused material.

--- ersilia/materials/instrumentation/definition.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad import *
-output_module_import_statements = [
-    'from abjad import *',
-    ]
-
-
-instrumentation = instrumenttools.InstrumentationSpecifier()
--- ersilia/materials/instrumentation/output.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-from abjad import *
-
-
-instrumentation = instrumenttools.InstrumentationSpecifier(
-    performers=instrumenttools.PerformerInventory(
-        []
-        ),
-    )
