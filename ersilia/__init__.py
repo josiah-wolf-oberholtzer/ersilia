033a5b2c725f0813955f05ad5716300f1682c328
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2014-12-28 22:47:24 -0800

    Cleaned up various things.

--- ersilia/__init__.py
+++ ersilia/__init__.py
@@ -1,3 +1,3 @@
 # -*- encoding: utf-8 -*-
-from ersilia import makers
 from ersilia import materials
+from ersilia.makers import *
---
+++ ersilia/materials/permitted_time_signatures/output.py
@@ -0,0 +1,4 @@
+# -*- encoding: utf-8 -*-
+
+
+permitted_time_signatures = None
