95e50b22d3d32b5be18109ae057bd49a8d77f1b0
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-03-16 08:42:22 -0700

    Revised permitted time signatures.

--- ersilia/materials/permitted_time_signatures/definition.py
+++ ersilia/materials/permitted_time_signatures/definition.py
@@ -2,4 +2,14 @@
 from abjad import *
 
 
-permitted_time_signatures = None
+permitted_time_signatures = indicatortools.TimeSignatureInventory([
+    (2, 4),
+    (3, 4),
+    (3, 8),
+    (4, 4),
+    (4, 8),
+    (5, 4),
+    (5, 8),
+    (6, 8),
+    (7, 8),
+    ])
--- ersilia/materials/permitted_time_signatures/output.py
+++ ersilia/materials/permitted_time_signatures/output.py
@@ -1,4 +1,17 @@
 # -*- encoding: utf-8 -*-
+from abjad.tools import indicatortools
 
 
-permitted_time_signatures = None
+permitted_time_signatures = indicatortools.TimeSignatureInventory(
+    [
+        indicatortools.TimeSignature((2, 4)),
+        indicatortools.TimeSignature((3, 4)),
+        indicatortools.TimeSignature((3, 8)),
+        indicatortools.TimeSignature((4, 4)),
+        indicatortools.TimeSignature((4, 8)),
+        indicatortools.TimeSignature((5, 4)),
+        indicatortools.TimeSignature((5, 8)),
+        indicatortools.TimeSignature((6, 8)),
+        indicatortools.TimeSignature((7, 8)),
+        ]
+    )
