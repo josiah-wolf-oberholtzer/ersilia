3a01fd91e13ccb5db8e699062de0fb807858ec50
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-03-16 08:40:21 -0700

    Added piano tremoli music specifier.

---
+++ ersilia/materials/piano_tremoli_music_specifier/definition.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+import consort
+from abjad.tools import selectortools
+from abjad.tools import spannertools
+
+
+piano_tremoli_music_specifier = consort.MusicSpecifier(
+    attachment_handler=consort.AttachmentHandler(
+        dynamics=consort.DynamicExpression(
+            dynamic_tokens='p ppp p ppp p ppp f mf f',
+            ),
+        stem_tremolo_spanner=consort.AttachmentExpression(
+            attachments=spannertools.StemTremoloSpanner(),
+            selector=selectortools.selects_pitched_runs(),
+            ),
+        ),
+    labels='pedaled',
+    pitch_handler=consort.AbsolutePitchHandler(
+        logical_tie_expressions=(
+            consort.ChordExpression(
+                chord_expr=(-7, -3, 0, 5, 6, 12),
+                ),
+            consort.ChordExpression(
+                chord_expr=(-7, -3, 0, 1, 5, 12),
+                ),
+            ),
+        pitch_specifier=consort.PitchSpecifier(
+            pitch_segments=(
+                "d'",
+                "f'",
+                "d'",
+                ),
+            ratio=(1, 1, 1),
+            ),
+        ),
+    )
---
+++ ersilia/materials/piano_tremoli_music_specifier/output.py
@@ -0,0 +1,114 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import datastructuretools
+from abjad.tools import mathtools
+from abjad.tools import pitchtools
+from abjad.tools import scoretools
+from abjad.tools import selectortools
+from abjad.tools import spannertools
+import consort
+
+
+piano_tremoli_music_specifier = consort.tools.MusicSpecifier(
+    attachment_handler=consort.tools.AttachmentHandler(
+        dynamics=consort.tools.AttachmentExpression(
+            attachments=datastructuretools.TypedList(
+                [
+                    consort.tools.DynamicExpression(
+                        dynamic_tokens=datastructuretools.CyclicTuple(
+                            [
+                                'p',
+                                'ppp',
+                                'p',
+                                'ppp',
+                                'p',
+                                'ppp',
+                                'f',
+                                'mf',
+                                'f',
+                                ]
+                            ),
+                        transitions=datastructuretools.CyclicTuple(
+                            [None]
+                            ),
+                        ),
+                    ]
+                ),
+            ),
+        stem_tremolo_spanner=consort.tools.AttachmentExpression(
+            attachments=datastructuretools.TypedList(
+                [
+                    spannertools.StemTremoloSpanner(),
+                    ]
+                ),
+            selector=selectortools.Selector(
+                callbacks=(
+                    selectortools.PrototypeSelectorCallback(
+                        prototype=scoretools.Leaf,
+                        ),
+                    selectortools.RunSelectorCallback(
+                        prototype=(
+                            scoretools.Note,
+                            scoretools.Chord,
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    labels=('pedaled',),
+    pitch_handler=consort.tools.AbsolutePitchHandler(
+        logical_tie_expressions=datastructuretools.CyclicTuple(
+            [
+                consort.tools.ChordExpression(
+                    chord_expr=pitchtools.IntervalSegment(
+                        (
+                            pitchtools.NumberedInterval(-7),
+                            pitchtools.NumberedInterval(-3),
+                            pitchtools.NumberedInterval(0),
+                            pitchtools.NumberedInterval(5),
+                            pitchtools.NumberedInterval(6),
+                            pitchtools.NumberedInterval(12),
+                            ),
+                        item_class=pitchtools.NumberedInterval,
+                        ),
+                    ),
+                consort.tools.ChordExpression(
+                    chord_expr=pitchtools.IntervalSegment(
+                        (
+                            pitchtools.NumberedInterval(-7),
+                            pitchtools.NumberedInterval(-3),
+                            pitchtools.NumberedInterval(0),
+                            pitchtools.NumberedInterval(1),
+                            pitchtools.NumberedInterval(5),
+                            pitchtools.NumberedInterval(12),
+                            ),
+                        item_class=pitchtools.NumberedInterval,
+                        ),
+                    ),
+                ]
+            ),
+        pitch_specifier=consort.tools.PitchSpecifier(
+            pitch_segments=(
+                pitchtools.PitchSegment(
+                    (
+                        pitchtools.NamedPitch("d'"),
+                        ),
+                    item_class=pitchtools.NamedPitch,
+                    ),
+                pitchtools.PitchSegment(
+                    (
+                        pitchtools.NamedPitch("f'"),
+                        ),
+                    item_class=pitchtools.NamedPitch,
+                    ),
+                pitchtools.PitchSegment(
+                    (
+                        pitchtools.NamedPitch("d'"),
+                        ),
+                    item_class=pitchtools.NamedPitch,
+                    ),
+                ),
+            ratio=mathtools.Ratio(1, 1, 1),
+            ),
+        ),
+    )
