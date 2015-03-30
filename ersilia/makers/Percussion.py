a9dfcb9cc694788fb313376d4823a3de2d7fab57
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-03-29 20:21:31 -0700

    Added Percussion maker.

---
+++ ersilia/makers/Percussion.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+from abjad.tools import pitchtools
+
+
+class Percussion(object):
+
+    HIGH_TOM = pitchtools.NamedPitch("c'")
+    MID_HIGH_TOM = pitchtools.NamedPitch("c'")
+    MID_LOW_TOM = pitchtools.NamedPitch("c'")
+    LOW_TOM = pitchtools.NamedPitch("c'")
+
+    TAM_TAM = pitchtools.NamedPitch("c'")
+    BASS_DRUM = pitchtools.NamedPitch("c'")
