a0d9a76c269cf54b13a71a12ab718c5f4ec20dea
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-02 11:42:27 -0700

    Added tutti timespan maker.

---
+++ ersilia/materials/tutti_timespan_maker/definition.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+import consort
+from abjad.tools import durationtools
+from abjad.tools import rhythmmakertools
+
+
+tutti_timespan_maker = consort.TaleaTimespanMaker(
+    playing_talea=rhythmmakertools.Talea(
+        counts=(4, 5, 4, 3, 7, 6),
+        denominator=8,
+        ),
+    playing_groupings=(3, 4, 2, 2, 3, 5),
+    repeat=True,
+    silence_talea=rhythmmakertools.Talea(
+        counts=(3, 4, 2, 5, 6, 9),
+        denominator=4,
+        ),
+    step_anchor=Right,
+    synchronize_groupings=True,
+    synchronize_step=True,
+    timespan_specifier=consort.TimespanSpecifier(
+        minimum_duration=durationtools.Duration(1, 4),
+        ),
+    )
