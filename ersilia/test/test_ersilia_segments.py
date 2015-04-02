ea485a2e8573498ebf5ee725b03e052eadde4109
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-01 23:05:51 -0700

    Modernized Ersilia.

--- ersilia/test/test_ersilia_segments.py
+++ ersilia/test/test_ersilia_segments.py
@@ -44,19 +44,22 @@ def test_ersilia_segments_01(segment_path):
     if os.path.exists(local_boilerplate_path):
         os.remove(local_boilerplate_path)
     with systemtools.FilesystemState(
+        keep=[illustration_ly_path, illustration_pdf_path],
         remove=[local_boilerplate_path],
         ):
         shutil.copyfile(boilerplate_path, local_boilerplate_path)
+        ide.idetools.AbjadIDE._replace_in_file(
+            local_boilerplate_path,
+            'PREVIOUS_SEGMENT_METADATA_IMPORT_STATEMENT',
+            'previous_segment_metadata = None',
+            )
         assert os.path.exists(local_boilerplate_path)
         assert not os.path.exists(illustration_candidate_ly_path)
         assert not os.path.exists(illustration_candidate_pdf_path)
-
         command = 'python {}'.format(local_boilerplate_path)
         exit_status = systemtools.IOManager.spawn_subprocess(command)
         assert exit_status == 0
-
         assert os.path.exists(illustration_candidate_ly_path)
         assert os.path.exists(illustration_candidate_pdf_path)
-
         shutil.move(illustration_candidate_ly_path, illustration_ly_path)
         shutil.move(illustration_candidate_pdf_path, illustration_pdf_path)
