97d5df38aba42f50e83e086d268194100a9d1ce4
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-02 22:33:44 -0700

    Positioned percussion pitches.

--- ersilia/makers/Percussion.py
+++ ersilia/makers/Percussion.py
@@ -4,10 +4,24 @@ from abjad.tools import pitchtools
 
 class Percussion(object):
 
-    HIGH_TOM = pitchtools.NamedPitch("c'")
-    MID_HIGH_TOM = pitchtools.NamedPitch("c'")
-    MID_LOW_TOM = pitchtools.NamedPitch("c'")
-    LOW_TOM = pitchtools.NamedPitch("c'")
+    CASTANET = pitchtools.NamedPitch("g''")
+    GUIRO = pitchtools.NamedPitch("e''")
+    TAMBOURINE = pitchtools.NamedPitch("c''")
 
-    TAM_TAM = pitchtools.NamedPitch("c'")
-    BASS_DRUM = pitchtools.NamedPitch("c'")
+    WOOD_BLOCK_5 = pitchtools.NamedPitch("g'")
+    TOM_4 = pitchtools.NamedPitch("f'")
+    WOOD_BLOCK_4 = pitchtools.NamedPitch("e'")
+    TOM_3 = pitchtools.NamedPitch("d'")
+    WOOD_BLOCK_3 = pitchtools.NamedPitch("c'")
+    TOM_2 = pitchtools.NamedPitch("b")
+    WOOD_BLOCK_2 = pitchtools.NamedPitch("a")
+    TOM_1 = pitchtools.NamedPitch("g")
+    WOOD_BLOCK_1 = pitchtools.NamedPitch("f")
+
+    SNARE_DRUM = pitchtools.NamedPitch("c")
+    TAM_TAM = pitchtools.NamedPitch("a,")
+    BASS_DRUM = pitchtools.NamedPitch("f,")
+
+    # MARIMBA
+
+    # CROTALES
