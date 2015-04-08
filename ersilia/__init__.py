0060d5b7de702c447fcabc78dd78f445be8d3987
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-07 19:14:06 -0700

    Registered \vstrut with LilyPondParser.

--- ersilia/__init__.py
+++ ersilia/__init__.py
@@ -1,4 +1,9 @@
 # -*- encoding: utf-8 -*-
+
+from abjad.tools import lilypondparsertools
+lilypondparsertools.LilyPondParser.register_markup_function('vstrut', [])
+del lilypondparsertools
+
 from ersilia.makers import *
 from ersilia import makers
 from ersilia.materials import *
