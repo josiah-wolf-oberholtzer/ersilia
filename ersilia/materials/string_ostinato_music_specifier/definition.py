6a4e37d0bef7879e796b5af8ba4e85f6c344c93d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-09 23:37:49 -0700

    Added more material templates.

---
+++ ersilia/materials/string_ostinato_music_specifier/definition.py
@@ -0,0 +1,5 @@
+# -*- encoding: utf-8 -*-
+from abjad import *
+
+
+string_ostinato_music_specifier = None
---
+++ ersilia/materials/wind_ostinato_music_specifier/definition.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+import consort
+from abjad.tools import rhythmmakertools
+
+
+wind_ostinato_music_specifier = consort.MusicSpecifier(
+    attachment_handler=consort.AttachmentHandler(
+        ),
+    color=None,
+    labels=[],
+    pitch_handler=consort.AbsolutePitchHandler(
+        ),
+    rhythm_maker=rhythmmakertools.NoteRhythmMaker(
+        tie_specifier=rhythmmakertools.TieSpecifier(
+            tie_across_divisions=True,
+            ),
+        ),
+    )
