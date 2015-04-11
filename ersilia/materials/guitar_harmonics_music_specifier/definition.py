44b5fb4c3c0ab8f2abdd0a1d39dfd908c70c7aec
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-11 00:18:14 -0700

    Revised guitar harmonics music.

--- ersilia/materials/guitar_harmonics_music_specifier/definition.py
+++ ersilia/materials/guitar_harmonics_music_specifier/definition.py
@@ -1,18 +1,43 @@
 # -*- encoding: utf-8 -*-
 import consort
 from abjad.tools import rhythmmakertools
+from abjad.tools import scoretools
 
 
 guitar_harmonics_music_specifier = consort.MusicSpecifier(
     attachment_handler=consort.AttachmentHandler(
+        dynamic_expressions=consort.DynamicExpression(
+            dynamic_tokens='p mp',
+            ),
         ),
     color=None,
     labels=[],
     pitch_handler=consort.AbsolutePitchHandler(
+        logical_tie_expressions=[
+            consort.HarmonicExpression('P4'),
+            consort.HarmonicExpression('P5'),
+            consort.HarmonicExpression('P8'),
+            consort.HarmonicExpression('P5'),
+            ],
         ),
-    rhythm_maker=rhythmmakertools.NoteRhythmMaker(
-        tie_specifier=rhythmmakertools.TieSpecifier(
-            tie_across_divisions=True,
+    rhythm_maker=consort.CompositeRhythmMaker(
+        default=rhythmmakertools.EvenDivisionRhythmMaker(
+            burnish_specifier=rhythmmakertools.BurnishSpecifier(
+                left_classes=[scoretools.Rest],
+                left_counts=[1, 0],
+                right_classes=[scoretools.Rest],
+                right_counts=[1],
+                ),
+            denominators=[8, 8, 16],
+            extra_counts_per_division=[0, 0, 1, 0, 1, 2],
+            ),
+        last=rhythmmakertools.IncisedRhythmMaker(
+            incise_specifier=rhythmmakertools.InciseSpecifier(
+                fill_with_notes=False,
+                prefix_counts=[1],
+                prefix_talea=[1],
+                talea_denominator=8,
+                ),
             ),
         ),
     )
--- ersilia/materials/guitar_harmonics_music_specifier/output.py
+++ ersilia/materials/guitar_harmonics_music_specifier/output.py
@@ -1,15 +1,70 @@
 # -*- encoding: utf-8 -*-
+from abjad.tools import datastructuretools
+from abjad.tools import pitchtools
 from abjad.tools import rhythmmakertools
+from abjad.tools import scoretools
 import consort
 
 
 guitar_harmonics_music_specifier = consort.tools.MusicSpecifier(
-    attachment_handler=consort.tools.AttachmentHandler(),
+    attachment_handler=consort.tools.AttachmentHandler(
+        dynamic_expressions=consort.tools.AttachmentExpression(
+            attachments=datastructuretools.TypedList(
+                [
+                    consort.tools.DynamicExpression(
+                        dynamic_tokens=datastructuretools.CyclicTuple(
+                            ['p', 'mp']
+                            ),
+                        transitions=datastructuretools.CyclicTuple(
+                            [None]
+                            ),
+                        ),
+                    ]
+                ),
+            ),
+        ),
     labels=(),
-    pitch_handler=consort.tools.AbsolutePitchHandler(),
-    rhythm_maker=rhythmmakertools.NoteRhythmMaker(
-        tie_specifier=rhythmmakertools.TieSpecifier(
-            tie_across_divisions=True,
+    pitch_handler=consort.tools.AbsolutePitchHandler(
+        logical_tie_expressions=datastructuretools.CyclicTuple(
+            [
+                consort.tools.HarmonicExpression(
+                    touch_interval=pitchtools.NamedInterval('+P4'),
+                    ),
+                consort.tools.HarmonicExpression(
+                    touch_interval=pitchtools.NamedInterval('+P5'),
+                    ),
+                consort.tools.HarmonicExpression(
+                    touch_interval=pitchtools.NamedInterval('+P8'),
+                    ),
+                consort.tools.HarmonicExpression(
+                    touch_interval=pitchtools.NamedInterval('+P5'),
+                    ),
+                ]
+            ),
+        ),
+    rhythm_maker=consort.tools.CompositeRhythmMaker(
+        default=rhythmmakertools.EvenDivisionRhythmMaker(
+            denominators=(8, 8, 16),
+            extra_counts_per_division=(0, 0, 1, 0, 1, 2),
+            burnish_specifier=rhythmmakertools.BurnishSpecifier(
+                left_classes=(
+                    scoretools.Rest,
+                    ),
+                right_classes=(
+                    scoretools.Rest,
+                    ),
+                left_counts=(1, 0),
+                right_counts=(1,),
+                ),
+            preferred_denominator='from_counts',
+            ),
+        last=rhythmmakertools.IncisedRhythmMaker(
+            incise_specifier=rhythmmakertools.InciseSpecifier(
+                prefix_talea=(1,),
+                prefix_counts=(1,),
+                talea_denominator=8,
+                fill_with_notes=False,
+                ),
             ),
         ),
     )
