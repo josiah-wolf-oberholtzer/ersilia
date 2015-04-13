8f17ca496aeaf7e27e33c76092062388387b86f7
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-13 13:32:28 -0700

    Added many valid time signatures.

--- ersilia/materials/permitted_time_signatures/definition.py
+++ ersilia/materials/permitted_time_signatures/definition.py
@@ -3,10 +3,14 @@ from abjad import *
 
 
 permitted_time_signatures = indicatortools.TimeSignatureInventory([
+    (2, 4),
+    (4, 8),
     (5, 8),
     (6, 8),
-    (7, 8),
-    (2, 4),
     (3, 4),
+    (7, 8),
     (4, 4),
+    (9, 8),
+    (5, 4),
+    (6, 4),
     ])
--- ersilia/materials/permitted_time_signatures/output.py
+++ ersilia/materials/permitted_time_signatures/output.py
@@ -4,11 +4,15 @@ from abjad.tools import indicatortools
 
 permitted_time_signatures = indicatortools.TimeSignatureInventory(
     [
+        indicatortools.TimeSignature((2, 4)),
+        indicatortools.TimeSignature((4, 8)),
         indicatortools.TimeSignature((5, 8)),
         indicatortools.TimeSignature((6, 8)),
-        indicatortools.TimeSignature((7, 8)),
-        indicatortools.TimeSignature((2, 4)),
         indicatortools.TimeSignature((3, 4)),
+        indicatortools.TimeSignature((7, 8)),
         indicatortools.TimeSignature((4, 4)),
+        indicatortools.TimeSignature((9, 8)),
+        indicatortools.TimeSignature((5, 4)),
+        indicatortools.TimeSignature((6, 4)),
         ]
     )
