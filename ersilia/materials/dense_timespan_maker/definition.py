6f1382752e7ce4a9eee26a5c1db0b2d3a75dca24
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-04-13 13:32:08 -0700

    No timespan-maker should have 16 as a talea denominator.

--- ersilia/materials/dense_timespan_maker/definition.py
+++ ersilia/materials/dense_timespan_maker/definition.py
@@ -5,18 +5,18 @@ from abjad import *
 
 dense_timespan_maker = consort.TaleaTimespanMaker(
     initial_silence_talea=rhythmmakertools.Talea(
-        counts=(0, 3, 4, 2, 5),
-        denominator=16,
+        counts=(0, 1, 2, 1, 3),
+        denominator=8,
         ),
     playing_talea=rhythmmakertools.Talea(
-        counts=(6, 8, 4, 5, 6, 6, 4),
-        denominator=16,
+        counts=(3, 4, 2, 2, 3, 3, 2),
+        denominator=8,
         ),
     playing_groupings=(2, 1, 2, 3, 1, 1, 2, 2),
     repeat=True,
     silence_talea=rhythmmakertools.Talea(
-        counts=(2, 4, 6, 3, 4, 10),
-        denominator=16,
+        counts=(1, 2, 3, 1, 2, 5),
+        denominator=8,
         ),
     step_anchor=Right,
     synchronize_groupings=False,
