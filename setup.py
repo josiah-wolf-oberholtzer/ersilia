652d76b4059c12a9ff068d20256a201d0aabc204
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-05-05 19:17:18 -0700

    Updating dependencies.

--- setup.py
+++ setup.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
 
-install_requires = (
-    'abjad',
-    'consort',
-    )
 
-def main():
+if __name__ == '__main__':
     setup(
         author='Josiah Wolf Oberholtzer',
         author_email='josiah.oberholtzer@gmail.com',
-        install_requires=install_requires,
+        install_requires=['consort'],
         name='ersilia',
-        packages=('ersilia',),
+        packages=['ersilia'],
         url='https://github.com/josiah-wolf-oberholtzer/ersilia',
         version='0.1',
         zip_safe=False,
         )
-
-
-if __name__ == '__main__':
-    main()
