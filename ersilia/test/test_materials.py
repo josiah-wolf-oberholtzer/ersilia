55b3b2e9a0fa89107696ecd97dae94d1551c20b7
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-05-05 20:23:41 -0700

    Updating tests.

--- ersilia/test/test_materials.py
+++ ersilia/test/test_materials.py
@@ -1,42 +1,21 @@
 # -*- encoding: utf-8 -*-
-import importlib
-import os
 import pytest
-import ide
+import pathlib
+from abjad.tools import commandlinetools
+from abjad.tools import systemtools
 
 
-abjad_ide = ide.tools.idetools.AbjadIDE()
-this_file = os.path.abspath(__file__)
-test_directory = os.path.dirname(this_file)
-inner_score_directory = os.path.dirname(test_directory)
-outer_score_directory = os.path.dirname(inner_score_directory)
-composer_scores_directory = os.path.dirname(outer_score_directory)
-# Travis monkey patch
-abjad_ide._configuration._composer_scores_directory_override = \
-    composer_scores_directory
-score_name = os.path.basename(abjad_ide._to_score_directory(this_file))
-materials_directory = abjad_ide._to_score_directory(this_file, 'materials')
-material_directories = abjad_ide._list_visible_paths(materials_directory)
-materials_module = importlib.import_module('{}.materials'.format(score_name))
+test_path = pathlib.Path(__file__).parent
+materials_path = (test_path / '..' / 'materials').resolve()
+material_names = [
+    path.name for path in materials_path.iterdir()
+    if path.is_dir()
+    ]
 
 
-@pytest.mark.parametrize('material_directory', material_directories)
-def test_materials_01(material_directory):
-    r'''Checks material definition files.
-    '''
-    abjad_ide.check_definition_file(material_directory)
-
-
-@pytest.mark.parametrize('material_directory', material_directories)
-def test_materials_02(material_directory):
-    r'''Makes material PDFs.
-    '''
-    material_package_name = os.path.basename(material_directory)
-    material = getattr(materials_module, material_package_name)
-    if not hasattr(material, '__illustrate__'):
-        return
-    illustrate_file = os.path.join(material_directory, '__illustrate__.py')
-    if not os.path.exists(illustrate_file):
-        abjad_ide.make_illustrate_file(material_directory)
-    success = abjad_ide.make_pdf(material_directory, subroutine=True)
-    assert success
+@pytest.mark.parametrize('material_name', material_names)
+def test_materials(material_name):
+    with systemtools.TemporaryDirectoryChange(str(test_path)):
+        script = commandlinetools.ManageMaterialScript()
+        command = ['--render', material_name]
+        script(command)
--- ersilia/test/test_segments.py
+++ ersilia/test/test_segments.py
@@ -1,30 +1,21 @@
 # -*- encoding: utf-8 -*-
-import os
 import pytest
-import ide
+import pathlib
+from abjad.tools import commandlinetools
+from abjad.tools import systemtools
 
 
-abjad_ide = ide.tools.idetools.AbjadIDE()
-this_file = os.path.abspath(__file__)
-test_directory = os.path.dirname(this_file)
-inner_score_directory = os.path.dirname(test_directory)
-outer_score_directory = os.path.dirname(inner_score_directory)
-composer_scores_directory = os.path.dirname(outer_score_directory)
-# Travis monkey patch
-abjad_ide._configuration._composer_scores_directory_override = \
-    composer_scores_directory
-segments_directory = abjad_ide._to_score_directory(this_file, 'segments')
-segment_directories = abjad_ide._list_visible_paths(segments_directory)
+test_path = pathlib.Path(__file__).parent
+segments_path = (test_path / '..' / 'segments').resolve()
+segment_names = [
+    path.name for path in segments_path.iterdir()
+    if path.is_dir()
+    ]
 
 
-@pytest.mark.parametrize('segment_directory', segment_directories)
-def test_segments_01(segment_directory):
-    r'''Checks segment definition files.'''
-    abjad_ide.check_definition_file(segment_directory)
-
-
-@pytest.mark.parametrize('segment_directory', segment_directories)
-def test_segments_02(segment_directory):
-    r'''Makes segments PDFs.'''
-    success = abjad_ide.make_pdf(segment_directory, subroutine=True)
-    assert success
+@pytest.mark.parametrize('segment_name', segment_names)
+def test_segments(segment_name):
+    with systemtools.TemporaryDirectoryChange(str(test_path)):
+        script = commandlinetools.ManageSegmentScript()
+        command = ['--render', segment_name]
+        script(command)
