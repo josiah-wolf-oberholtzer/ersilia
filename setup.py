58033a7a481a09206b53f7d663965124e1490c4d
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2017-08-12 13:30:03 -0700

    Pin to Consort==0.1.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     setup(
         author='Josiah Wolf Oberholtzer',
         author_email='josiah.oberholtzer@gmail.com',
-        install_requires=['consort'],
+        install_requires=['consort==0.1'],
         name='ersilia',
         packages=['ersilia'],
         url='https://github.com/josiah-wolf-oberholtzer/ersilia',
