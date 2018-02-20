0c2786860043c251c8a3348f11846a8262e2b92b
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-02-20 10:40:44 -0500

    Pin Sphinx version.

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ if __name__ == '__main__':
     setup(
         author='Josiah Wolf Oberholtzer',
         author_email='josiah.oberholtzer@gmail.com',
-        install_requires=['consort==0.1'],
+        install_requires=[
+            'consort==0.1',
+            'Sphinx==1.6.7',
+        ],
         name='ersilia',
         packages=['ersilia'],
         url='https://github.com/josiah-wolf-oberholtzer/ersilia',
